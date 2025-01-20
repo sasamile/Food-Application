9fc2d054337a8fe828f16113cc01f0f7353cc801
sasamile <nspes2020@gmail.com>
2025-01-20 17:12:37 -0500

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="w-full h-full flex justify-center items-center">
+      <Text className="text-2xl text-red-500">Testing 123</Text>
+    </View>
+  );
+}
+
+
